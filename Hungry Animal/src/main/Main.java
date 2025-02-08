18fd2a9037ffe8ecd3e3151a12efd0cdcfd7f64e
PrimalSunflower <pattersoncarson60@gmail.com>
2025-02-07 19:33:07 -0500

    something
    something something

--- Hungry Animal/src/main/Main.java
+++ Hungry Animal/src/main/Main.java
@@ -3,6 +3,6 @@ package main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("You have gotten the project successfully!");
-		System.out.println("I carson do not agree to the terms");
+		System.out.println("I carson do agree to the terms!");
 	}
 }
